777af95a474a3403e0281c18e3e3bdac678568e7
Oliver Sander <sander@dune-project.org>
2009-05-31 13:32:27 +0000

    document the descriptor base classes
    [[Imported from SVN: r26]]

--- glue/surfaces/SurfaceDescriptor.hh
+++ glue/surfaces/SurfaceDescriptor.hh
@@ -20,24 +20,36 @@
 #define SURFACEDESCRIPTOR_HH_
 
 
+/** \brief Base class for element-selecting predicates
+    \tparam GV GridView that the elements are selected from
+ */
 template<typename GV>
 class ElementDescriptor
 {
 public:
-
+  /** \brief Return true if the element pointed to be eptr should be extracted */
   virtual bool contains(const typename GV::Traits::template Codim<0>::EntityPointer& eptr) const = 0;
 
+  /** \brief Dummy virtual destructor */
   virtual ~ElementDescriptor() {}
 };
 
 
+/** \brief Base class for face-selecting predicates
+    \tparam GV GridView that the faces are selected from
+ */
 template<typename GV>
 class FaceDescriptor
 {
 public:
 
+  /** \brief Return true if a face should be extracted.  Faces are specified through an element and a face number
+     \param eptr An element
+     \param face Face number
+   */
   virtual bool contains(const typename GV::Traits::template Codim<0>::EntityPointer& eptr, unsigned int face) const = 0;
 
+  /** \brief Dummy virtual destructor */
   virtual ~FaceDescriptor() {}
 };
 
