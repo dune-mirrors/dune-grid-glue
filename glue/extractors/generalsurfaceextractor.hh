c826b34563502e86861ba598a2807e8baa088d26
Oliver Sander <sander@dune-project.org>
2009-09-21 09:37:16 +0000

    methods geometry() and geometryLocal()
    [[Imported from SVN: r274]]

--- glue/extractors/generalsurfaceextractor.hh
+++ glue/extractors/generalsurfaceextractor.hh
@@ -91,6 +91,12 @@ public:
 
 public:
 
+  using Codim1Extractor<GV>::codim;
+
+  typedef Dune::GenericGeometry::BasicGeometry<dim-codim, Dune::GenericGeometry::DefaultGeometryTraits<ctype,dim-codim,dimworld> > Geometry;
+
+  typedef Dune::GenericGeometry::BasicGeometry<dim-codim, Dune::GenericGeometry::DefaultGeometryTraits<ctype,dim-codim,dim> > LocalGeometry;
+
   /*  C O N S T R U C T O R S   A N D   D E S T R U C T O R S  */
 
   /**
@@ -128,6 +134,13 @@ public:
    * @param descr a descriptor that "selects" the faces to add to the surface
    */
   void update(const FaceDescriptor<GV>& descr);
+
+
+  /** \brief Get world geometry of the extracted face */
+  Geometry geometry(unsigned int index) const;
+
+  /** \brief Get geometry of the extracted face in element coordinates */
+  LocalGeometry geometryLocal(unsigned int index) const;
 
 
   /**
@@ -455,6 +468,44 @@ void GeneralSurfaceExtractor<GV>::update(const FaceDescriptor<GV>& descr)
 }
 
 
+/** \brief Get World geometry of the extracted face */
+template<typename GV>
+typename GeneralSurfaceExtractor<GV>::Geometry GeneralSurfaceExtractor<GV>::geometry(unsigned int index) const
+{
+  std::vector<Coords> corners(simplex_corners);
+  for (int i = 0; i < simplex_corners; ++i)
+    corners[i] = this->_coords[this->_faces[index].corners[i].idx].coord;
+
+  return Geometry(Dune::GeometryType(Dune::GeometryType::simplex,dim-codim), corners);
+}
+
+
+/** \brief Get Geometry of the extracted face in element coordinates */
+template<typename GV>
+typename GeneralSurfaceExtractor<GV>::LocalGeometry GeneralSurfaceExtractor<GV>::geometryLocal(unsigned int index) const
+{
+  std::vector<Coords> corners(simplex_corners);
+
+  unsigned int num_in_self = this->indexInInside(index);
+  Dune::GeometryType gt = this->_elmtInfo.find(this->_faces[index].parent)->second->p->type();
+
+  // computing the locals is straight forward for flat rectangles,
+  // we only need the triangle's corners in element coordinate
+  if (this->_faces[index].category <= 1) {
+    // the triangle's corners are (0 1 2) using face indices
+    for (int i = 0; i < simplex_corners; ++i)
+      corners[i] = cornerLocalInRefElement<ctype, dimworld>(gt, num_in_self, i);
+
+  } else {
+    // the triangle's corners are (3 2 1) using face indices
+    for (int i = 0; i < simplex_corners; ++i)
+      corners[i] = cornerLocalInRefElement<ctype, dimworld>(gt, num_in_self, 3-i);
+  }
+
+  return LocalGeometry(Dune::GeometryType(Dune::GeometryType::simplex,dim-codim), corners);
+}
+
+
 template<typename GV>
 inline void GeneralSurfaceExtractor<GV>::globalCoords(unsigned int index, const Coords &bcoords, Coords &wcoords) const
 {
@@ -551,6 +602,9 @@ void GeneralSurfaceExtractor<GV>::localAndGlobalCoords(unsigned int index,
                                                        Dune::array<Dune::FieldVector<ctype,dim>, dimworld> &ecoords,
                                                        Dune::array<Dune::FieldVector<ctype,dimworld>, dimworld> &wcoords) const
 {
+  Geometry worldGeometry = geometry(index);
+  LocalGeometry localGeometry = geometryLocal(index);
+
   Dune::array<Dune::FieldVector<ctype,dim>, dimworld> barycentricCoords;
   for (int i=0; i<dimworld; i++)
     barycentricCoords[i] = referenceToBarycentric(subEntityCoords[i]);
@@ -564,6 +618,11 @@ void GeneralSurfaceExtractor<GV>::localAndGlobalCoords(unsigned int index,
     ElementPtr eptr = this->_elmtInfo.find(this->_faces[index].parent)->second->p;
     for (size_t i = 0; i < ecoords.size(); ++i)
       ecoords[i] = eptr->geometry().local(wcoords[i]);
+  }
+
+  for (size_t i = 0; i < wcoords.size(); ++i) {
+    assert( (ecoords[i] - localGeometry.global(subEntityCoords[i])).two_norm() < 1e-6);
+    assert( (wcoords[i] - worldGeometry.global(subEntityCoords[i])).two_norm() < 1e-6);
   }
 }
 
