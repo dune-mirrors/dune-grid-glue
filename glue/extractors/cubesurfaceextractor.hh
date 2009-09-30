77b1384a52effe1a17c15a1852acdbaf34d95367
Christian Engwer <christi@dune-project.org>
2009-09-30 08:46:08 +0000

    cleanup cubeextractor
    [[Imported from SVN: r311]]

--- glue/extractors/cubesurfaceextractor.hh
+++ glue/extractors/cubesurfaceextractor.hh
@@ -149,141 +149,124 @@ public:
 template<typename GV>
 void CubeSurfaceExtractor<GV>::update(const FaceDescriptor<GV>& descr)
 {
-  // free everything there is in this object
-  this->clear();
+  // Currently the extractor splits (if necessary) quadrilaterals into triangles (because psurface)
+  // can only handle triangles). Since this is done manually we cannot handle grids
+  // of dimension higher than 3.
+  dune_static_assert(dim<=3, "CubeSurfaceExtractor only implemented for 1d, 2d, 3d");
 
-  // In this first pass iterate over all entities of codim 0.
   // For each codim 1 intersection check if it is part of the boundary and if so,
   // get its corner vertices, find resp. store them together with their associated index,
   // and remember the indices of the boundary faces' corners.
 
+  // free everything there is in this object
+  this->clear();
+
+  // several counter for consecutive indexing are needed
+  size_t simplex_index = 0;
+  size_t vertex_index = 0;
+
+  // a temporary container where newly acquired face
+  // information can be stored at first
+  std::deque<FaceInfo> temp_faces;
+
+  // iterate over all codim 0 elemets on the grid
+  for (ElementIter elit = this->_gv.template begin<0, PType>();
+       elit != this->_gv.template end<0, PType>(); ++elit)
   {
-    // several counter for consecutive indexing are needed
-    int simplex_index = 0;
-    int vertex_index = 0;
-    IndexType eindex = 0;             // supress warning
+    // count number of selected faces for this element
+    size_t simplex_count = 0;
 
-    // needed later for insertion into a std::set which only
-    // works with const references
+    IndexType eindex = this->indexSet().template index<0>(*elit);
 
-    // a temporary container where newly acquired face
-    // information can be stored at first
-    std::deque<FaceInfo> temp_faces;
-
-    // iterate over all codim 0 elemets on the grid
-    for (ElementIter elit = this->_gv.template begin<0, PType>();
-         elit != this->_gv.template end<0, PType>(); ++elit)
+    // iterate over all intersections of codim 1 and test if the
+    // boundary intersections are to be added to the surface
+    for (IsIter is = this->_gv.ibegin(*elit); is != this->_gv.iend(*elit); ++is)
     {
-      // remember the indices of the faces that shall become
-      // part of the surface
-      std::set<int> boundary_faces;
+      // only look at boundary faces
+      if (is->boundary() && descr.contains(elit, is->indexInInside())) {
 
-      // iterate over all intersections of codim 1 and test if the
-      // boundary intersections are to be added to the surface
-      for (IsIter is = this->_gv.ibegin(*elit); is != this->_gv.iend(*elit); ++is)
-      {
-        // only look at boundary faces
-        if (is->boundary() && descr.contains(elit, is->indexInInside())) {
+        // Make sure the face is a cube
+        if (!is->type().isCube())
+          DUNE_THROW(Dune::GridError, "found non-cube boundary entity: " << is->type());
 
-          // Make sure the face is a cube
-          if (!is->type().isCube())
-            DUNE_THROW(Dune::GridError, "found non-cube boundary entity: " << is->type());
+        unsigned int face_index = is->indexInInside();
 
-          boundary_faces.insert(is->indexInInside());
+        // now we only have to care about the 3D case, i.e. the quadrilateral
+        // face has to be divided into two triangles
+        unsigned int vertex_indices[4];
+        unsigned int vertex_numbers[4];
 
-        }
-      }
-
-      // if some face is part of the surface add it!
-      if (boundary_faces.size() != 0)
-      {
-        dune_static_assert(dim == 2 || dim == 3, "CubeSurfaceExtractor works only for 2D and 3D");
-        static const int factor = (dim == 2 ? 1 : 2);
-        // add an entry to the element info map, the index will be set properly later,
-        // whereas the number of faces is already known
-        eindex = this->indexSet().template index<0>(*elit);
-        this->_elmtInfo[eindex] = new typename Codim1Extractor<GV>::ElementInfo(simplex_index, elit, factor * boundary_faces.size());
-
-        // now add the faces in ascending order of their indices
-        // (we are only talking about 1-4 faces here, so O(n^2) is ok!)
-        for (typename std::set<int>::const_iterator sit = boundary_faces.begin(); sit != boundary_faces.end(); ++sit)
+        // get the vertex pointers for the quadrilateral's corner vertices
+        // and try for each of them whether it is already inserted or not
+        for (int i = 0; i < cube_corners; ++i)
         {
-          // now we only have to care about the 3D case, i.e. the quadrilateral
-          // face has to be divided into two triangles
+          // get the number of the vertex in the parent element
+          vertex_numbers[i] = orientedSubface<dim>(elit->type(), face_index, i);
 
-          unsigned int vertex_indices[4];
-          unsigned int vertex_numbers[4];
+          // get the vertex pointer and the index from the index set
+          VertexPtr vptr(elit->template subEntity<dim>(vertex_numbers[i]));
+          IndexType vindex = this->indexSet().template index<dim>(*vptr);
 
-          // get the vertex pointers for the quadrilateral's corner vertices
-          // and try for each of them whether it is already inserted or not
-          for (int i = 0; i < cube_corners; ++i)
+          // if the vertex is not yet inserted in the vertex info map
+          // it is a new one -> it will be inserted now!
+          typename Codim1Extractor<GV>::VertexInfoMap::iterator vimit = this->_vtxInfo.find(vindex);
+          if (vimit == this->_vtxInfo.end())
           {
-            // get the number of the vertex in the parent element
-            vertex_numbers[i] = orientedSubface<dim>(elit->type(), *sit, i);
-
-            // get the vertex pointer and the index from the index set
-            VertexPtr vptr(elit->template subEntity<dim>(vertex_numbers[i]));
-            IndexType vindex = this->indexSet().template index<dim>(*vptr);
-
-            // if the vertex is not yet inserted in the vertex info map
-            // it is a new one -> it will be inserted now!
-            typename Codim1Extractor<GV>::VertexInfoMap::iterator vimit = this->_vtxInfo.find(vindex);
-            if (vimit == this->_vtxInfo.end())
-            {
-              // insert into the map
-              this->_vtxInfo[vindex] = new typename Codim1Extractor<GV>::VertexInfo(vertex_index, vptr);
-              // remember this vertex' index
-              vertex_indices[i] = vertex_index;
-              // increase the current index
-              vertex_index++;
-            }
-            else
-            {
-              // only remember the vertex' index
-              vertex_indices[i] = vimit->second->idx;
-            }
+            // insert into the map
+            this->_vtxInfo[vindex] = new typename Codim1Extractor<GV>::VertexInfo(vertex_index, vptr);
+            // remember this vertex' index
+            vertex_indices[i] = vertex_index;
+            // increase the current index
+            vertex_index++;
           }
+          else
+          {
+            // only remember the vertex' index
+            vertex_indices[i] = vimit->second->idx;
+          }
+        }
 
-          // Currently the extractor splits quadrilaterals into triangles (because psurface)
-          // can only handle triangles). Since this is done manually we cannot handle grids
-          // of dimension higher than 3.
-          dune_static_assert(dim<=3, "CubeSurfaceExtractor only implemented for 1d, 2d, 3d");
+        // add a new face to the temporary collection
+        simplex_count ++;
+        temp_faces.push_back(
+          FaceInfo(simplex_index, eindex, face_index, FaceInfo::first));
+        for (int i=0; i<dim; i++) {
+          temp_faces.back().corners[i].idx = vertex_indices[i];
+          // remember the vertices' numbers in parent element's vertices
+          temp_faces.back().corners[i].num = vertex_numbers[i];
+        }
 
-          // add a new face to the temporary collection
+        // now introduce the second triangle subdividing the quadrilateral
+        // ATTENTION: the order of vertices given by "orientedSubface" corresponds to the order
+        // of a Dune quadrilateral, i.e. the triangles are given by 0 1 2 and 3 2 1
+        if (dim==3) {
+          // add a new face to the temporary collection for the second tri
+          simplex_count++;
           temp_faces.push_back(
-            FaceInfo(simplex_index++, eindex, *sit, FaceInfo::first));
-          for (int i=0; i<dim; i++) {
-            temp_faces.back().corners[i].idx = vertex_indices[i];
-            // remember the vertices' numbers in parent element's vertices
-            temp_faces.back().corners[i].num = vertex_numbers[i];
-          }
+            FaceInfo(simplex_index+1, eindex, face_index, FaceInfo::second));
+          temp_faces.back().corners[0].idx = vertex_indices[3];
+          temp_faces.back().corners[1].idx = vertex_indices[2];
+          temp_faces.back().corners[2].idx = vertex_indices[1];
+          // remember the vertices' numbers in parent element's vertices
+          temp_faces.back().corners[0].num = vertex_numbers[3];
+          temp_faces.back().corners[1].num = vertex_numbers[2];
+          temp_faces.back().corners[2].num = vertex_numbers[1];
+        }
+      }        // end if is contained
 
-          // now introduce the second triangle subdividing the quadrilateral
-          // ATTENTION: the order of vertices given by "orientedSubface" corresponds to the order
-          // of a Dune quadrilateral, i.e. the triangles are given by 0 1 2 and 3 2 1
-          if (dim==3) {
-            // add a new face to the temporary collection for the second tri
-            temp_faces.push_back(
-              FaceInfo(simplex_index++, eindex, *sit, FaceInfo::second));
-            temp_faces.back().corners[0].idx = vertex_indices[3];
-            temp_faces.back().corners[1].idx = vertex_indices[2];
-            temp_faces.back().corners[2].idx = vertex_indices[1];
-            // remember the vertices' numbers in parent element's vertices
-            temp_faces.back().corners[0].num = vertex_numbers[3];
-            temp_faces.back().corners[1].num = vertex_numbers[2];
-            temp_faces.back().corners[2].num = vertex_numbers[1];
-          }
+    }             // end loop over intersections
 
-        }                         // end loop over found surface parts
-      }
-    }             // end loop over elements
+    // add element entry with the offset of the first simplex and the number of simplices
+    if (simplex_count != 0)
+      this->_elmtInfo[eindex] = new typename Codim1Extractor<GV>::ElementInfo(simplex_index, elit, simplex_count);
+    simplex_index += simplex_count;
 
-    // allocate the array for the face specific information...
-    this->_faces.resize(simplex_index);
-    // ...and fill in the data from the temporary containers
-    copy(temp_faces.begin(), temp_faces.end(), this->_faces.begin());
-  }
+  }       // end loop over elements
 
+  // allocate the array for the face specific information...
+  this->_faces.resize(simplex_index);
+  // ...and fill in the data from the temporary containers
+  copy(temp_faces.begin(), temp_faces.end(), this->_faces.begin());
 
   // now first write the array with the coordinates...
   this->_coords.resize(this->_vtxInfo.size());
