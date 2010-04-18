54448d25642fcfa0ff63351175f383e21c7d4478
Oliver Sander <sander@dune-project.org>
2010-04-18 10:59:50 +0000

    surfacedescriptor.hh is now called extractorpredicates.hh
    [[Imported from SVN: r472]]

--- dune/grid-glue/test/mixeddimcouplingtest.cc
+++ dune/grid-glue/test/mixeddimcouplingtest.cc
@@ -9,7 +9,7 @@
 #include <dune/grid/sgrid.hh>
 #include <dune/grid/common/quadraturerules.hh>
 
-#include <dune/grid-glue/extractors/surfacedescriptor.hh>
+#include <dune/grid-glue/extractors/extractorpredicates.hh>
 #include <dune/grid-glue/extractors/gridextractiontraits.hh>
 #include <dune/grid-glue/merging/psurfacemerge.hh>
 #include <dune/grid-glue/adapter/gridglue.hh>
