ec2eb0c4c5a694c019c76741f6d68d46a8ee7864
Christian Engwer <christi@dune-project.org>
2010-11-30 22:43:51 +0000

    disable debug output ... now the tests are as good as 2 weeks ago
    [[Imported from SVN: r584]]

--- dune/grid-glue/common/orientedsubface.hh
+++ dune/grid-glue/common/orientedsubface.hh
@@ -33,49 +33,49 @@ int orientedSubface(const Dune::GeometryType& type, int face, int vertex)
   // Triangle
   if (type.isTriangle() && face==1)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex+1)%2, dim);
   }
 
   // Quadrilateral
   if (type.isQuadrilateral() && (face==0 || face==2))
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex+1)%2, dim);
   }
 
   // Tetrahedron, face 0 and 2:  swap local vertices 1 and 2
   if (type.isTetrahedron() && (face==0 || face==2) && vertex>=1)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex%2)+1, dim);
   }
 
   // Pyramid, quad face (0):  swap 1 and 2
   if (type.isPyramid() && face==0 && vertex>=1 && vertex<=2)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex%2)+1, dim);
   }
 
   // Pyramid, faces 1 and 4: swap 0 and 1
   if (type.isPyramid() && (face==1 || face==4) && vertex<2)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex+1)%2, dim);
   }
 
   // Prism, face 3: swap vertices 1 and 2
   if (type.isPrism() && face==3 && vertex>=1)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex%2)+1, dim);
   }
 
   // Prism: face 1: swap vertices 0 and 1; and 2 and 3
   if (type.isPrism() && face==1)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return (vertex<2)
            ? refElement.subEntity(face,1, (vertex+1)%2, dim)
            : refElement.subEntity(face,1, ((vertex-1)%2)+2, dim);
@@ -86,7 +86,7 @@ int orientedSubface(const Dune::GeometryType& type, int face, int vertex)
   // face 3: 2 3 6 7 --> 3 2 7 6
   if (type.isHexahedron() && (face==0 || face==3))
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     return refElement.subEntity(face,1, (vertex<2) ? (vertex+1)%2 : ((vertex-1)%2)+2, dim);
   }
 
@@ -94,7 +94,7 @@ int orientedSubface(const Dune::GeometryType& type, int face, int vertex)
   // face 4: 0 1 2 3 --> 0 2 1 3
   if (type.isHexahedron() && face==4)
   {
-    std::cout << type << " CHANGE ORIENTATION\n";
+    // std::cout << type << " CHANGE ORIENTATION\n";
     const int renumber[4] = {0,2,1,3};
     return refElement.subEntity(face,1, renumber[vertex], dim);
   }
