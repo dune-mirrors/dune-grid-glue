ff4be06ae5dfc2af3190cfdc83ff923cf0cf6e1a
Christian Engwer <christi@dune-project.org>
2013-03-13 18:18:12 +0000

    [test] adapt communication test to - check that the required information are available - only use those information, suitable for out communication direction
    [[Imported from SVN: r804]]

--- dune/grid-glue/test/communicationtest.hh
+++ dune/grid-glue/test/communicationtest.hh
@@ -3,33 +3,62 @@
 #ifndef GRIDGLUE_COMMTEST_HH
 #define GRIDGLUE_COMMTEST_HH
 
-template <typename ctype, int dimw>
+template <typename ctype, int dimw, bool forward>
 class CheckGlobalCoordDataHandle :
-  public Dune::GridGlue::CommDataHandle< CheckGlobalCoordDataHandle<ctype, dimw>, Dune::FieldVector<ctype,dimw> >
+  public Dune::GridGlue::CommDataHandle< CheckGlobalCoordDataHandle<ctype, dimw, forward>, Dune::FieldVector<ctype,dimw> >
 {
+  template<class RISType>
+  bool hasSender (RISType& i) const
+  {
+    if (forward)
+      return i.self();
+    else
+      return i.neighbor();
+  }
+  template<class RISType>
+  bool hasRecipient (RISType& i) const
+  {
+    if (forward)
+      return i.neighbor();
+    else
+      return i.self();
+  }
 public:
   template<class RISType>
   size_t size (RISType& i) const
   {
-    return i.geometry().corners();
+    assert(hasSender(i));
+    if (forward)
+      return i.geometry().corners();
+    else
+      return i.geometryOutside().corners();
   }
 
   template<class MessageBuffer, class EntityType, class RISType>
   void gather (MessageBuffer& buff, const EntityType& e, const RISType & i) const
   {
-    for (size_t n=0; n<size(i); n++)
-      buff.write(i.geometry().corner(n));
+    assert(hasSender(i));
+    if (forward)
+      for (size_t n=0; n<size(i); n++)
+        buff.write(i.geometry().corner(n));
+    else
+      for (size_t n=0; n<size(i); n++)
+        buff.write(i.geometryOutside().corner(n));
   }
 
   template<class MessageBuffer, class EntityType, class RISType>
   void scatter (MessageBuffer& buff, const EntityType& e, const RISType & i, size_t n)
   {
+    assert(hasRecipient(i));
     assert(n == size(i));
     for (size_t n=0; n<size(i); n++)
     {
       Dune::FieldVector<ctype,dimw> x;
       buff.read(x);
-      assert( (x - i.geometry().corner(n)).two_norm() < 1e-6 );
+      if (forward)
+        assert( (x - i.geometry().corner(n)).two_norm() < 1e-6 );
+      else
+        assert( (x - i.geometryOutside().corner(n)).two_norm() < 1e-6 );
     }
   }
 };
@@ -39,9 +68,10 @@ void testCommunication (const GlueType& glue)
 {
   typedef typename GlueType::ctype ctype;
   enum { dimw = GlueType::dimworld };
-  CheckGlobalCoordDataHandle<ctype, dimw> dh;
-  glue.communicate(dh, Dune::All_All_Interface, Dune::ForwardCommunication);
-  glue.communicate(dh, Dune::All_All_Interface, Dune::BackwardCommunication);
+  CheckGlobalCoordDataHandle<ctype, dimw, true> dh_forward;
+  CheckGlobalCoordDataHandle<ctype, dimw, false> dh_backward;
+  glue.communicate(dh_forward, Dune::All_All_Interface, Dune::ForwardCommunication);
+  glue.communicate(dh_backward, Dune::All_All_Interface, Dune::BackwardCommunication);
 }
 
 #endif // GRIDGLUE_COMMTEST_HH
