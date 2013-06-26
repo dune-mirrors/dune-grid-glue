5786bcc8ab40da2a24bb2e3c9a1141377a2f8a4a
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-06-26 16:28:39 +0200

    Implement overlapping intersections between 1d and 2d elements
    This is not thoroughly tested yet.  Use at your own risk.

--- dune/grid-glue/merging/mixeddimoverlappingmerge.hh
+++ dune/grid-glue/merging/mixeddimoverlappingmerge.hh
@@ -10,9 +10,10 @@
 
 #include <dune/common/fmatrix.hh>
 #include <dune/common/fvector.hh>
+#include <dune/common/version.hh>
 
 #include <dune/geometry/referenceelements.hh>
-#include <dune/geometry/genericgeometry/geometry.hh>
+#include <dune/geometry/multilineargeometry.hh>
 
 #include <dune/grid/common/grid.hh>
 
@@ -72,10 +73,289 @@ private:
                            unsigned int grid2Index,
                            std::bitset<(1<<dim2)>& neighborIntersects2)
   {
-    std::cout << "MixedDimOverlappingMerge::computeIntersection: Not implemented yet!" << std::endl;
+#if DUNE_VERSION_NEWER(DUNE_GEOMETRY,2,3)
+    const Dune::ReferenceElement<T,dim1>& refElement1 = Dune::ReferenceElements<T,dim1>::general(grid1ElementType);
+    const Dune::ReferenceElement<T,dim2>& refElement2 = Dune::ReferenceElements<T,dim2>::general(grid2ElementType);
+#else
+    const Dune::GenericReferenceElement<T,dim1>& refElement1 = Dune::GenericReferenceElements<T,dim1>::general(grid1ElementType);
+    const Dune::GenericReferenceElement<T,dim2>& refElement2 = Dune::GenericReferenceElements<T,dim2>::general(grid2ElementType);
+#endif
+
+    // A few consistency checks
+    assert((unsigned int)(refElement1.size(dim1)) == grid1ElementCorners.size());
+    assert((unsigned int)(refElement2.size(dim2)) == grid2ElementCorners.size());
+
+    // Make generic geometries representing the grid1- and grid2 element.
+    // this eases computation of local coordinates.
+    typedef Dune::CachedMultiLinearGeometry<T, dim1, dimworld> Geometry1;
+    typedef Dune::CachedMultiLinearGeometry<T, dim2, dimworld> Geometry2;
+
+    Geometry1 grid1Geometry(grid1ElementType, grid1ElementCorners);
+    Geometry2 grid2Geometry(grid2ElementType, grid2ElementCorners);
+
+    if (dim1==1 and dim2==2 and dimworld==2)
+    {
+      assert(grid1ElementCorners.size() == 2);  // line segment
+
+      // Compute intersections of segment with all edges of the 2d element
+      std::vector<Dune::FieldVector<T,2> > intersections;
+      for (int i=0; i<refElement2.size(1); i++)
+      {
+        Dune::array<Dune::FieldVector<T,2>, 2> edge;
+        edge[0] = grid2ElementCorners[refElement2.subEntity(i,1,0,2)];
+        edge[1] = grid2ElementCorners[refElement2.subEntity(i,1,1,2)];
+        segmentSegmentIntersection2D(grid1ElementCorners,edge,intersections);
+      }
+
+      // Compute whether the segment endpoints are contained in the triangle
+      std::bitset<2> inTriangle("00");
+
+      for (size_t i=0; i<2; i++)
+        inTriangle[i] = Dune::GenericReferenceElements<T,dim2>::general(grid2ElementType).checkInside(grid2Geometry.local(grid1ElementCorners[i]));
+
+      switch (intersections.size()) {
+
+          case 0:
+            if (inTriangle[0] and inTriangle[1])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = 0;
+              this->intersections_.back().grid1Local_[1] = 1;
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = grid2Geometry.local(grid1ElementCorners[0]);
+              this->intersections_.back().grid2Local_[1] = grid2Geometry.local(grid1ElementCorners[1]);
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+            }
+            return;
+
+          case 1:
+          {
+            if (inTriangle[0])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = 0;
+              this->intersections_.back().grid1Local_[1] = grid1Geometry.local(intersections[0]);
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = grid2Geometry.local(grid1ElementCorners[0]);
+              this->intersections_.back().grid2Local_[1] = grid2Geometry.local(intersections[0]);
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+
+            } else if (inTriangle[1])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = grid1Geometry.local(intersections[0]);
+              this->intersections_.back().grid1Local_[1] = 1;
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = grid2Geometry.local(intersections[0]);
+              this->intersections_.back().grid2Local_[1] = grid2Geometry.local(grid1ElementCorners[1]);
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+
+            }
+
+            return;
+
+
+          }
+
+
+          case 2:
+          case 3:
+          {
+            this->intersections_.push_back(RemoteSimplicialIntersection());
+
+            // Compute local coordinates in the grid1 element
+            this->intersections_.back().grid1Local_[0] = grid1Geometry.local(intersections[0]);
+            this->intersections_.back().grid1Local_[1] = grid1Geometry.local(intersections[1]);
+
+            // Compute local coordinates in the grid2 element
+            this->intersections_.back().grid2Local_[0] = grid2Geometry.local(intersections[0]);
+            this->intersections_.back().grid2Local_[1] = grid2Geometry.local(intersections[1]);
+
+            // Set indices
+            this->intersections_.back().grid1Entity_ = grid1Index;
+            this->intersections_.back().grid2Entity_ = grid2Index;
+
+            return;
+
+
+          }
+
+      }
+
+    } else if (dim1==2 and dim2==1 and dimworld==2)
+    {
+      assert(grid2ElementCorners.size() == 2);  // line segment
+
+      // Compute intersections of segment with all edges of the 2d element
+      std::vector<Dune::FieldVector<T,2> > intersections;
+      for (int i=0; i<refElement1.size(1); i++)
+      {
+        Dune::array<Dune::FieldVector<T,2>, 2> edge;
+        edge[0] = grid1ElementCorners[refElement1.subEntity(i,1,0,2)];
+        edge[1] = grid1ElementCorners[refElement1.subEntity(i,1,1,2)];
+        segmentSegmentIntersection2D(grid2ElementCorners,edge,intersections);
+      }
+
+      // Compute whether the segment endpoints are contained in the triangle
+      std::bitset<2> inTriangle("00");
+
+      for (size_t i=0; i<2; i++)
+        inTriangle[i] = Dune::GenericReferenceElements<T,dim1>::general(grid1ElementType).checkInside(grid1Geometry.local(grid2ElementCorners[i]));
+
+      switch (intersections.size()) {
+
+          case 0:
+            if (inTriangle[0] and inTriangle[1])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = grid1Geometry.local(grid2ElementCorners[0]);
+              this->intersections_.back().grid1Local_[1] = grid1Geometry.local(grid2ElementCorners[1]);
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = 0;
+              this->intersections_.back().grid2Local_[1] = 1;
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+            }
+            return;
+
+          case 1:
+          {
+            if (inTriangle[0])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = grid1Geometry.local(grid2ElementCorners[0]);
+              this->intersections_.back().grid1Local_[1] = grid1Geometry.local(intersections[0]);
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = 0;
+              this->intersections_.back().grid2Local_[1] = grid2Geometry.local(intersections[0]);
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+
+            } else if (inTriangle[1])
+            {
+              this->intersections_.push_back(RemoteSimplicialIntersection());
+
+              // Compute local coordinates in the grid1 element
+              this->intersections_.back().grid1Local_[0] = grid1Geometry.local(intersections[0]);
+              this->intersections_.back().grid1Local_[1] = grid1Geometry.local(grid2ElementCorners[1]);
+
+              // Compute local coordinates in the grid2 element
+              this->intersections_.back().grid2Local_[0] = grid2Geometry.local(intersections[0]);
+              this->intersections_.back().grid2Local_[1] = 1;
+
+              // Set indices
+              this->intersections_.back().grid1Entity_ = grid1Index;
+              this->intersections_.back().grid2Entity_ = grid2Index;
+
+            }
+
+            return;
+
+
+          }
+
+
+          case 2:
+          case 3:
+          {
+            this->intersections_.push_back(RemoteSimplicialIntersection());
+
+            // Compute local coordinates in the grid1 element
+            this->intersections_.back().grid1Local_[0] = grid1Geometry.local(intersections[0]);
+            this->intersections_.back().grid1Local_[1] = grid1Geometry.local(intersections[1]);
+
+            // Compute local coordinates in the grid2 element
+            this->intersections_.back().grid2Local_[0] = grid2Geometry.local(intersections[0]);
+            this->intersections_.back().grid2Local_[1] = grid2Geometry.local(intersections[1]);
+
+            // Set indices
+            this->intersections_.back().grid1Entity_ = grid1Index;
+            this->intersections_.back().grid2Entity_ = grid2Index;
+
+            return;
+
+
+          }
+
+      }
+
+    } else
+      std::cout << "MixedDimOverlappingMerge for dim1 = " << dim1 << ", dim2 = "
+                                                          << dim2 << ", dimworld = "
+                                                          << dimworld << ": Not implemented yet!" << std::endl;
   }
 
+  static void segmentSegmentIntersection2D( const std::vector<Dune::FieldVector<T,dimworld> >& X,
+                                              const Dune::array<Dune::FieldVector<T,dimworld>, 2>& edge,
+                                              std::vector<Dune::FieldVector<T,dimworld> > & P );
+
 };
+
+template<int dim1, int dim2, int dimworld, typename T>
+void MixedDimOverlappingMerge<dim1, dim2, dimworld, T>::segmentSegmentIntersection2D( const std::vector<Dune::FieldVector<T,dimworld> >& X,
+                                                    const Dune::array<Dune::FieldVector<T,dimworld>, 2>& Y,
+                                                    std::vector<Dune::FieldVector<T,dimworld> > & P )
+{
+
+  // get size_type for all the vectors we are using
+  typedef typename std::vector<Dune::Empty>::size_type size_type;
+
+  Dune::FieldVector<T,dimworld>  p,r ;
+  Dune::FieldMatrix<T,dimworld,dimworld>  A ;
+
+  int i=0;
+  int j=0;
+
+      Dune::FieldVector<T,dimworld> B = Y[j] - X[i] ;
+
+      int I = (i+1)%3 ;
+      int J = (j+1)%3 ;
+
+      A[0][0] =  X[I][0] - X[i][0] ;  A[1][0] =  X[I][1] - X[i][1] ;
+      A[0][1] =  Y[j][0] - Y[J][0] ;  A[1][1] =  Y[j][1] - Y[J][1] ;
+
+      if (A.determinant()!=0) {
+
+        A.solve(r,B) ;
+
+        if ((r[0]>=0)&&(r[0]<=1)&&(r[1]>=0)&&(r[1]<=1)) {
+          p = X[I] - X[i] ;
+          p *= r[0] ;
+          p += X[i] ;
+          P.push_back(p);
+        }
+      }
+
+}
+
 
 }  // namespace Dune
 
