055ba55d70a4e50f96abd68f7b3620039c8dd43f
Jonathan Youett <youett@math.fu-berlin.de>
2014-03-11 15:12:16 +0100

    Add new merger implementation ContactMerge
    This merger should be used in the same situations where PSurfaceMerge
    has been used previously.  However, it does not depend on libpsurface.
    Instead, it is a standalone implementation based on StandardMerge.
    This should be faster and more robust and using PSurfaceMerge.

---
+++ dune/grid-glue/merging/contactmerge.cc
@@ -0,0 +1,358 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#include <dune/grid-glue/common/projectionhelper.hh>
+
+template<int dim, int dimworld, typename T>
+void ContactMerge<dim, dimworld, T>::computeIntersection(const Dune::GeometryType& grid1ElementType,
+                                   const std::vector<Dune::FieldVector<T,dimworld> >& grid1ElementCorners,
+                                   unsigned int grid1Index,
+                                   std::bitset<(1<<dim)>& neighborIntersects1,
+                                   const Dune::GeometryType& grid2ElementType,
+                                   const std::vector<Dune::FieldVector<T,dimworld> >& grid2ElementCorners,
+                                   unsigned int grid2Index,
+                                   std::bitset<(1<<dim)>& neighborIntersects2)
+{
+    std::vector<Dune::array<LocalCoords,2> > polytopeCorners;
+
+    // Initialize
+    for (int i=0; i<(1<<dim); i++) {
+        neighborIntersects1[i] = false;
+        neighborIntersects2[i] = false;
+    }
+
+    const int nCorners1 = grid1ElementCorners.size();
+    const int nCorners2 = grid2ElementCorners.size();
+
+    // The grid1 projection directions
+    std::vector<WorldCoords> directions1(nCorners1);
+    for (size_t i=0; i<directions1.size(); i++)
+        directions1[i] = nodalDomainDirections_[this->grid1ElementCorners_[grid1Index][i]];
+
+    // The grid2 projection directions
+    // These are only needed in a check for validity of the projection and they should be chosen
+    // to be some outer pointing directions, e.g. outer normals
+    std::vector<WorldCoords> directions2(nCorners1);
+    for (size_t i=0; i<directions1.size(); i++)
+        directions2[i] = nodalTargetDirections_[this->grid2ElementCorners_[grid2Index][i]];
+
+    /////////////////////////////////////////////////////
+    //  Compute all corners of the intersection polytope
+    /////////////////////////////////////////////////////
+
+
+    // If we hit a corner of the grid1 element we store its index
+    std::vector<int> hitCorners(nCorners2,-1);
+    // If a corner was hit already we don't have to project it
+    std::vector<bool> skip(nCorners1);
+    std::vector<bool> proj(nCorners2);
+
+    // local coordinates
+    LocalCoords localCoords;
+    T eps = 1e-4;
+
+    // Compute which points of the grid1 element lie inside the grid2 element
+    for (size_t i=0; i<grid2ElementCorners.size(); i++)
+        if (Projection::template inverseProjection<dim,dimworld,T>(grid1ElementCorners, directions1,
+                                                                grid2ElementCorners[i], localCoords, overlap_)) {
+
+            // There are case where the intersection is not valid
+            WorldCoords base = interpolate(grid1ElementCorners,grid1ElementType,localCoords);
+            WorldCoords baseNormal = interpolate(directions1,grid1ElementType,localCoords);
+
+            WorldCoords segment = grid2ElementCorners[i] - base;
+            T distance = segment.two_norm();
+
+            // If both criterions are invalid
+            if ((segment*directions2[i]) > eps
+                 && (segment*baseNormal) < -eps)
+            {
+                // then the projection is still valid if the overlap is small
+                if (distance > overlap_)
+                    continue;
+
+            // if only one is invalid then the projection is invalid
+            } else if ((segment*directions2[i]) > eps
+                    || (segment*baseNormal) < -eps)
+                continue;
+
+            // corner could be projected
+            proj[i] = true;
+
+            Dune::array<LocalCoords,2> corner;
+            corner[1] = localCornerCoords(i,grid2ElementType);
+            corner[0] = localCoords;
+
+            polytopeCorners.push_back(corner);
+
+            hitCorners[i] = isCorner(grid1ElementType,localCoords);
+
+            if (hitCorners[i] != -1)
+                skip[hitCorners[i]]=true;
+        }
+
+    // Compute which points of the grid2 element lie inside the grid1 element
+    for (size_t i=0; i<grid1ElementCorners.size(); i++) {
+
+        if (skip[i])
+            continue;
+
+        if (Projection::template projection<dim,dimworld,T>(grid1ElementCorners[i], directions1[i],
+                                                                grid2ElementCorners, localCoords, overlap_)) {
+            Dune::array<LocalCoords,2> corner;
+            corner[0] = localCornerCoords(i,grid1ElementType);
+            corner[1] = localCoords;
+
+            // corner could be projected
+            skip[i] = true;
+
+            polytopeCorners.push_back(corner);
+        }
+    }
+
+    // check which neighbors might also intersect
+    const Dune::ReferenceElement<T,dim>& ref2 = Dune::ReferenceElements<T,dim>::general(grid2ElementType);
+    for (int i=0; i<ref2.size(1); i++) {
+
+        // if all face corners hit the the other element then
+        // the neighbor might also intersect
+
+        bool intersects(true);
+        for (int k=0; k<ref2.size(i,1,dim); k++)
+            intersects &= proj[ref2.subEntity(i,1,k,dim)];
+
+        if (intersects)
+            neighborIntersects2[i] = true;
+    }
+
+    const Dune::ReferenceElement<T,dim>& ref1 = Dune::ReferenceElements<T,dim>::general(grid1ElementType);
+    for (int i=0; i<ref1.size(1); i++) {
+
+        // if all face corners hit the the other element then
+        // the neighbor might also intersect
+
+        bool intersects(true);
+        for (int k=0; k<ref1.size(i,1,dim); k++)
+            intersects &= skip[ref1.subEntity(i,1,k,dim)];
+
+        if (intersects)
+            neighborIntersects1[i] = true;
+    }
+
+    // Compute the edge intersections
+    Projection::template addEdgeIntersections<dim,dimworld,T>(grid1ElementCorners,grid2ElementCorners,
+                                                              directions1, grid1ElementType,
+                                                              grid2ElementType, polytopeCorners, hitCorners,
+                                                              neighborIntersects1, neighborIntersects2, overlap_);
+
+    // remove possible doubles
+    removeDoubles(polytopeCorners);
+
+    // Compute an interior point of the polytope
+    int nPolyCorners = polytopeCorners.size();
+
+    // If the polytope is degenerated then there is no intersection
+    if (nPolyCorners<dimworld)
+        return;
+
+    // If the polytope is a simplex return it
+    if (nPolyCorners==dim+1) {
+
+     //   std::cout<<"Add intersection: 1\n";
+        typename Base::RemoteSimplicialIntersection intersect;
+        intersect.grid1Entity_ = grid1Index;
+        intersect.grid2Entity_ = grid2Index;
+
+        for (int j=0;j<dim+1; j++) {
+            intersect.grid1Local_[j]=polytopeCorners[j][0];
+            intersect.grid2Local_[j]=polytopeCorners[j][1];
+        }
+        this->intersections_.push_back(intersect);
+
+        return;
+    }
+
+    // At this point we must have dimworld>=3
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //  Compute a point in the middle of the polytope and order all corners cyclic
+    //////////////////////////////////////////////////////////////////////////////
+
+    Dune::array<LocalCoords,2> center;
+    center[0] = 0; center[1] = 0;
+    for (int i=0; i<nPolyCorners; i++) {
+        center[0].axpy(1.0/nPolyCorners,polytopeCorners[i][0]);
+        center[1].axpy(1.0/nPolyCorners,polytopeCorners[i][1]);
+    }
+
+    // Order cyclic
+    std::vector<int> ordering;
+    computeCyclicOrder(polytopeCorners,center[0],ordering);
+
+    //////////////////////////////////////
+    // Add intersections
+    ////////////////////////////////
+
+      //  std::cout<<"Add intersection: "<<polytopeCorners.size()<<"\n";
+    for (size_t i=0; i<polytopeCorners.size(); i++) {
+
+        typename Base::RemoteSimplicialIntersection intersect;
+        intersect.grid1Entity_ = grid1Index;
+        intersect.grid2Entity_ = grid2Index;
+
+        for (int j=0;j<dim; j++) {
+            intersect.grid1Local_[j]=polytopeCorners[ordering[(i+j)%nPolyCorners]][0];
+            intersect.grid2Local_[j]=polytopeCorners[ordering[(i+j)%nPolyCorners]][1];
+        }
+
+        // last corner is the center for all intersections
+        intersect.grid1Local_[dim]=center[0];
+        intersect.grid2Local_[dim]=center[1];
+
+        this->intersections_.push_back(intersect);
+    }
+}
+
+template<int dim, int dimworld, typename T>
+void ContactMerge<dim, dimworld, T>::computeCyclicOrder(const std::vector<Dune::array<LocalCoords,2> >& polytopeCorners,
+                        const LocalCoords& center, std::vector<int>& ordering) const
+{
+    ordering.resize(polytopeCorners.size());
+
+    for (size_t k=0; k<ordering.size(); k++)
+        ordering[k] = k;
+
+    //TODO Do I have to order triangles to get some correct orientation?
+    if (polytopeCorners.size()<=3)
+        return;
+
+    // compute angles inside the polygon plane w.r.t to this axis
+    LocalCoords  edge0 = polytopeCorners[0][0] - center;
+
+    // Compute a vector that is perpendicular to the edge but lies in the polytope plane
+    // So we have a unique ordering
+    LocalCoords  edge1 = polytopeCorners[1][0] - center;
+    LocalCoords normal0 = edge1;
+    normal0.axpy(-(edge0*edge1),edge0);
+
+    std::vector<T> angles(polytopeCorners.size());
+
+    for (size_t i=0; i<polytopeCorners.size(); i++) {
+
+        LocalCoords  edge = polytopeCorners[i][0] - center;
+
+        T x(edge*edge0);
+        T y(edge*normal0);
+
+        angles[i] = std::atan2(y, x);
+        if (angles[i]<0)
+            angles[i] += 2*M_PI;
+    }
+
+    // bubblesort
+
+    for (int i=polytopeCorners.size(); i>1; i--){
+        bool swapped = false;
+
+        for (int j=0; j<i-1; j++){
+
+            if (angles[j] > angles[j+1]){
+                swapped = true;
+                std::swap(angles[j], angles[j+1]);
+                std::swap(ordering[j], ordering[j+1]);
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+template<int dim, int dimworld, typename T>
+void ContactMerge<dim, dimworld, T>::setupNodalDirections(const std::vector<WorldCoords>& coords1,
+                              const std::vector<unsigned int>& elements1,
+                              const std::vector<Dune::GeometryType>& elementTypes1,
+                              const std::vector<WorldCoords>& coords2,
+                              const std::vector<unsigned int>& elements2,
+                              const std::vector<Dune::GeometryType>& elementTypes2)
+{
+    if (domainDirections_) {
+
+        // Sample the provided analytical contact direction field
+        nodalDomainDirections_.resize(coords1.size());
+        for (size_t i=0; i<coords1.size(); i++)
+            domainDirections_->evaluate(coords1[i], nodalDomainDirections_[i]);
+    } else
+        computeOuterNormalField(coords1,elements1,elementTypes1, nodalDomainDirections_);
+
+    if (targetDirections_) {
+
+        // Sample the provided analytical target direction field
+        nodalTargetDirections_.resize(coords2.size());
+        for (size_t i=0; i<coords2.size(); i++)
+            targetDirections_->evaluate(coords2[i], nodalTargetDirections_[i]);
+    } else
+        computeOuterNormalField(coords2,elements2,elementTypes2, nodalTargetDirections_);
+}
+
+template<int dim, int dimworld, typename T>
+void ContactMerge<dim, dimworld, T>::computeOuterNormalField(const std::vector<WorldCoords>& coords,
+                                const std::vector<unsigned int>& elements,
+                                const std::vector<Dune::GeometryType>& elementTypes,
+                                std::vector<WorldCoords>& normals)
+{
+    normals.assign(coords.size(),WorldCoords(0));
+
+
+    int offset = 0;
+
+    for (size_t i=0; i<elementTypes.size(); i++) {
+
+        int nCorners = Dune::ReferenceElements<T,dim>::general(elementTypes[i]).size(dim);
+
+        // For segments 1, for triangles or quadrilaterals take the first 2
+        std::vector<WorldCoords> edges(dim);
+        for (int j=1; j<=dim; j++)
+            edges[j-1] = coords[elements[offset + j]] - coords[elements[offset]];
+
+        WorldCoords elementNormal;
+
+        if (dim==1) {
+            elementNormal[0] = edges[0][1]; elementNormal[1] = -edges[0][0];
+        } else
+            elementNormal = Projection::crossProduct(edges[0],edges[1]);
+
+        elementNormal /= elementNormal.two_norm();
+
+        for (int j=0; j<nCorners;j++)
+            normals[elements[offset + j]] += elementNormal;
+
+        offset += nCorners;
+    }
+
+    for (size_t i=0; i<coords.size(); i++)
+        normals[i] /= normals[i].two_norm();
+}
+
+template<int dim, int dimworld, typename T>
+void ContactMerge<dim, dimworld, T>::removeDoubles(std::vector<Dune::array<LocalCoords,2> >& polytopeCorners)
+{
+
+    size_t counter(1);
+    for (size_t i=1; i<polytopeCorners.size(); i++) {
+        bool contained = false;
+        for (size_t j=0; j<counter; j++)
+            if ( (polytopeCorners[j][0]-polytopeCorners[i][0]).two_norm()<1e-10) {
+                assert((polytopeCorners[j][1]-polytopeCorners[i][1]).two_norm()<1e-10);
+                contained = true;
+                break;
+            }
+
+        if (!contained) {
+            if (counter < i)
+                polytopeCorners[counter] = polytopeCorners[i];
+            counter++;
+        }
+    }
+    polytopeCorners.resize(counter);
+}
---
+++ dune/grid-glue/merging/contactmerge.hh
@@ -0,0 +1,219 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/**
+  @file
+  @brief Merge two grid boundary surfaces that may be a positive distance apart
+ */
+
+#ifndef DUNE_GRID_GLUE_MERGING_CONTACTMERGE_HH
+#define DUNE_GRID_GLUE_MERGING_CONTACTMERGE_HH
+
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <limits>
+
+#include <dune/common/fvector.hh>
+#include <dune/common/function.hh>
+#include <dune/common/exceptions.hh>
+#include <dune/common/bitsetvector.hh>
+
+#include <dune/grid/common/grid.hh>
+
+#include <dune/grid-glue/merging/standardmerge.hh>
+#include <dune/grid-glue/adapter/gridglue.hh>
+#include <dune/grid-glue/extractors/extractorpredicate.hh>
+
+/** \brief Merge two grid boundary surfaces that may be a positive distance apart
+
+  \tparam dim Grid dimension of the coupling grids.  Must be the same for both sides
+  \tparam dimworld  Dimension of the world coordinates.  Must be equal to dim or to dim+1
+  \tparam T Type used for coordinates
+ */
+template<int dim, int dimworld, typename T = double>
+class ContactMerge
+: public StandardMerge<T,dim,dim,dimworld>
+{
+    dune_static_assert( dim==1 || dim==2,
+            "ContactMerge yet only handles the cases dim==1 and dim==2!");
+
+    dune_static_assert( dim==dimworld || dim+1==dimworld,
+            "ContactMerge yet only handles the cases dim==dimworld and dim+1==dimworld!");
+
+    typedef StandardMerge<T,dim,dim,dimworld> Base;
+public:
+
+    /*   E X P O R T E D   T Y P E S   A N D   C O N S T A N T S   */
+
+    /// @brief the numeric type used in this interface
+    typedef T ctype;
+
+    /// @brief the coordinate type used in this interface
+    typedef Dune::FieldVector<T, dimworld>  WorldCoords;
+
+    /// @brief the coordinate type used in this interface
+    typedef Dune::FieldVector<T, dim>  LocalCoords;
+
+    ContactMerge(const T allowedOverlap,
+            const Dune::VirtualFunction<WorldCoords,WorldCoords>* domainDirections = NULL,
+            const Dune::VirtualFunction<WorldCoords,WorldCoords>* targetDirections = NULL)
+        : domainDirections_(domainDirections), targetDirections_(targetDirections),
+          overlap_(allowedOverlap)
+    {}
+
+private:
+    /** \brief Vector field on the domain surface which prescribes the direction
+      in which the domain surface is projected onto the target surface
+     */
+    const Dune::VirtualFunction<WorldCoords,WorldCoords>* domainDirections_;
+    std::vector<WorldCoords> nodalDomainDirections_;
+
+    /** \brief Vector field on the target surface which prescribes a 'forward'
+      direction.
+
+      We use the normals of the target side to increase projection
+      robustness.  If these cannot be computed from the surface directly
+      (e.g. because it is not properly oriented), they can be given
+      explicitly through the targetDirections field.
+     */
+    const Dune::VirtualFunction<WorldCoords,WorldCoords>* targetDirections_;
+    std::vector<WorldCoords> nodalTargetDirections_;
+
+    //! Allow some overlap, i.e. also look in the negative projection directions
+    const T overlap_;
+
+    /**
+     * @brief Set surface direction functions
+     *
+     * The matching of the geometries offers the possibility to specify a function for
+     * the exact evaluation of domain surface normals. If no such function is specified
+     * (default) normals are interpolated.
+     * @param value the new function (or NULL to unset the function)
+     */
+    inline
+    void setSurfaceDirections(const Dune::VirtualFunction<WorldCoords,WorldCoords>* domainDirections,
+                              const Dune::VirtualFunction<WorldCoords,WorldCoords>* targetDirections)
+    {
+            domainDirections_ = domainDirections;
+            targetDirections_ = targetDirections;
+            this->valid = false;
+    }
+
+    /** \brief Compute the intersection between two overlapping elements
+     *
+     *   The result is a set of simplices.
+     */
+    void computeIntersection(const Dune::GeometryType& grid1ElementType,
+            const std::vector<Dune::FieldVector<T,dimworld> >& grid1ElementCorners,
+            unsigned int grid1Index,
+            std::bitset<(1<<dim)>& neighborIntersects1,
+            const Dune::GeometryType& grid2ElementType,
+            const std::vector<Dune::FieldVector<T,dimworld> >& grid2ElementCorners,
+            unsigned int grid2Index,
+            std::bitset<(1<<dim)>& neighborIntersects2);
+
+    /**
+      * @copydoc StandardMerge<T,grid1Dim,grid2Dim,dimworld>::build
+     */
+    void build(const std::vector<Dune::FieldVector<T,dimworld> >& grid1Coords,
+        const std::vector<unsigned int>& grid1Elements,
+        const std::vector<Dune::GeometryType>& grid1ElementTypes,
+        const std::vector<Dune::FieldVector<T,dimworld> >& grid2Coords,
+        const std::vector<unsigned int>& grid2Elements,
+        const std::vector<Dune::GeometryType>& grid2ElementTypes)
+    {
+        std::cout<<"ContactMerge building grid!\n";
+        // setup the nodal direction vectors
+        setupNodalDirections(grid1Coords, grid1Elements, grid1ElementTypes,
+                grid2Coords, grid2Elements, grid2ElementTypes);
+
+        Base::build(grid1Coords, grid1Elements, grid1ElementTypes,
+                   grid2Coords, grid2Elements, grid2ElementTypes);
+
+    }
+
+private:
+
+    //! Compute local coordinates of a corner
+    static LocalCoords localCornerCoords(int i, const Dune::GeometryType& gt)
+    {
+        const Dune::ReferenceElement<T,dim>& ref = Dune::ReferenceElements<T,dim>::general(gt);
+        return ref.position(i,dim);
+    }
+
+    //! Check if local coordinates correspond to a vertex and if yes returns its local index
+    static int isCorner(const Dune::GeometryType& gt, const LocalCoords& local)
+    {
+        const Dune::ReferenceElement<T,dim>& ref = Dune::ReferenceElements<T,dim>::general(gt);
+
+        for (int i=0; i<ref.size(dim); i++)
+            if ((ref.position(i,dim)-local).two_norm()<1e-8)
+                return i;
+        return -1;
+    }
+
+    //! Transform local to barycentric coordinates
+    static std::vector<T> localToBarycentric(const Dune::GeometryType& gt, const LocalCoords& local)
+    {
+        std::vector<T> bar(Dune::ReferenceElements<T,dim>::general(gt).size(dim));
+        if (bar.size()<4) {
+            bar[0] = 1.0;
+            for (int i=0; i<dim; i++) {
+                bar[i+1] = local[i];
+                bar[0] -= local[i];
+            }
+        } else {
+            // quadrilateral case
+            for (int i=0; i<4; i++) {
+                bar[i] = 1;
+
+                for (int j=0; j<dim; j++)
+                    // if j-th bit of i is set multiply with in[j], else with 1-in[j]
+                    bar[i] *= (i & (1<<j)) ? local[j] :  1-local[j];
+            }
+        }
+        return bar;
+    }
+
+    //! Compute global coordinates of a local point using linear interpolation of the corners
+    static WorldCoords interpolate(const std::vector<WorldCoords>& p,
+            const Dune::GeometryType& gt, const LocalCoords& local)
+    {
+        // Compute barycentric coordinates
+        std::vector<T> bar = localToBarycentric(gt, local);
+
+        WorldCoords global(0);
+        for (size_t i=0; i<p.size(); i++)
+            global.axpy(bar[i],p[i]);
+
+        return global;
+    }
+
+    //! Order the corners of the intersection polytope in cyclic order
+    void computeCyclicOrder(const std::vector<Dune::array<LocalCoords,2> >& polytopeCorners,
+            const LocalCoords& center, std::vector<int>& ordering) const;
+
+    //! Setup the direction vectors containing the directions for each vertex
+    void setupNodalDirections(const std::vector<WorldCoords>& coords1,
+            const std::vector<unsigned int>& elements1,
+            const std::vector<Dune::GeometryType>& elementTypes1,
+            const std::vector<WorldCoords>& coords2,
+            const std::vector<unsigned int>& elements2,
+            const std::vector<Dune::GeometryType>& elementTypes2);
+
+    //! If no direction field was specified compute the outer normal field
+    void computeOuterNormalField(const std::vector<WorldCoords>& coords,
+            const std::vector<unsigned int>& elements,
+            const std::vector<Dune::GeometryType>& elementTypes,
+            std::vector<WorldCoords>& normals);
+
+    //! Remove all multiples
+    void removeDoubles(std::vector<Dune::array<LocalCoords,2> >& polytopeCorners);
+};
+
+#include "contactmerge.cc"
+
+#endif // DUNE_GRID_GLUE_MERGING_CONTACTMERGE_HH
