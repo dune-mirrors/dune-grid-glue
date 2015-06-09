256a730388705c776509b97e358d0abfa1ac7954
Ansgar Burchardt <burchardt@igpm.rwth-aachen.de>
2015-06-09 14:04:02 +0200

    Add documentation for "range-based for" support.

--- dune/grid-glue/adapter/rangegenerators.hh
+++ dune/grid-glue/adapter/rangegenerators.hh
@@ -10,6 +10,9 @@
 namespace Dune {
 namespace GridGlue {
 
+/**
+ * Static tag representing reversal of in- and outside of intersecions.
+ */
 template<bool reverse>
 struct Reverse
   : std::integral_constant<bool, reverse>
@@ -20,6 +23,53 @@ struct Reverse
   Reverse<!reverse> operator!() const
     { return {}; }
 };
+
+#ifdef DOXYGEN
+
+/**
+ * Static tag representing reversal of in- and outside of intersections.
+ * \relates Reverse
+ */
+const Reverse<true> reversed;
+
+/**
+ * \brief Iterate over all intersections of a GridGlue.
+ *
+ * This function returns an object representing the range of intersections
+ * with respect to the GridGlue glue. Its main purpose is to enable iteration
+ * over these intersections by means of a range-based for loop:
+ *
+ * \code
+ * // Iterate over all intersections of a GridGlue in various ways
+ * using Dune::GridGlue::GridGlue;
+ * using Dune::GridGlue::Reverse;
+ * using Dune::GridGlue::reversed;
+ *
+ * GridGlue<...> glue;
+ * for (const auto& in : intersections(glue)) { ... }
+ * for (const auto& in : intersections(glue, reversed)) { ... }
+ * for (const auto& in : intersections(glue, !reversed)) { ... }
+ * for (const auto& in : intersections(glue, Reversed<true>())) { ... }
+ * \endcode
+ *
+ * The in- and outside of the intersection can be reversed by passing
+ * `reversed` as the second argument. The fourth form can be used in
+ * case a template parameter for reversal is required.
+ *
+ * \since dune-common 2.4
+ * \relatesalso Dune::GridGlue::GridGlue
+ * \param glue GridGlue to obtain the intersections from
+ * \param reverse Tag to indicate reversal of in- and outside of intersections
+ * \returns an unspecified object that is guaranteed to fulfill the interface
+ *          of IteratorRange and that can be iterated over using a range-based
+ *          for loop.
+ * \see Dune::GridGlue::Intersection
+ */
+template<...>
+IteratorRange<...>
+intersections(const GridGlue<...>& glue, const Reverse<...>& reverse = !reversed);
+
+#else
 
 namespace {
 const Reverse<true> reversed;
@@ -36,6 +86,8 @@ intersections(const GridGlue<P0, P1>& glue, const Reverse<reverse>& = {})
 }
 
 #endif
+
+#endif // DOXYGEN
 
 } /* namespace GridGlue */
 } /* namespace Dune */
