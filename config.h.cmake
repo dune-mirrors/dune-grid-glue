c835b9f6ab2c3853069e1fd8c02c41b6fd45f348
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2016-03-11 14:40:02 +0100

    config.h.cmake: Use correct version number.
    (cherry picked from commit 2242c762343de4c5ca492b8a397298aa38da756c)

--- config.h.cmake
+++ config.h.cmake
@@ -4,12 +4,12 @@
 #define DUNE_GRID_GLUE_VERSION "${DUNE_GRID_GLUE_VERSION}"
 
 /* Define to the major version of dune-grid-glue */
-#define DUNE_GRID_GLUE_VERSION_MAJOR ${DUNE_GRID_VERSION_MAJOR}
+#define DUNE_GRID_GLUE_VERSION_MAJOR ${DUNE_GRID_GLUE_VERSION_MAJOR}
 
 /* Define to the minor version of dune-grid-glue */
-#define DUNE_GRID_GLUE_VERSION_MINOR ${DUNE_GRID_VERSION_MINOR}
+#define DUNE_GRID_GLUE_VERSION_MINOR ${DUNE_GRID_GLUE_VERSION_MINOR}
 
 /* Define to the revision of dune-grid-glue */
-#define DUNE_GRID_GLUE_VERSION_REVISION ${DUNE_GRID_VERSION_REVISION}
+#define DUNE_GRID_GLUE_VERSION_REVISION ${DUNE_GRID_GLUE_VERSION_REVISION}
 
 /* end dune-grid-glue */
