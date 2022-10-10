6bc7849ad9003d3e4e9bc38ea1a5360b0eeda39f
Ansgar Burchardt <ansgar.burchardt@tu-dresden.de>
2022-10-10 08:22:20 +0200

    add SPDX information
    See https://reuse.software/ for a description.

--- dune/grid-glue/common/areawriter.hh
+++ dune/grid-glue/common/areawriter.hh
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-GPL-2.0-only-with-dune-grid-glue-exception
 #ifndef DUNE_GRIDGLUE_COMMON_AREAWRITER_HH
 #define DUNE_GRIDGLUE_COMMON_AREAWRITER_HH
 
